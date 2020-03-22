d88cf761295b83d10e5681e6c779983b60db551d
abhishekgupta97023 <abhishekgupta97023@gmail.com>
2020-03-22 17:31:29 +0530

    Initial commit

---
+++ app/src/main/java/com/abhishekgupta/list_in_list/models/Chapter.java
@@ -0,0 +1,11 @@
+package com.abhishekgupta.list_in_list.models;
+
+/**
+ * Created by abhishek on 06-05-2018.
+ */
+
+public class Chapter {
+    public int id;
+    public String chapterName;
+    public String imageUrl;
+}
