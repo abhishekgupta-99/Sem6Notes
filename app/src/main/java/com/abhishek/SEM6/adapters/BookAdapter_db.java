4c9a4ff086e893ac18c083a476681bf701f8ac84
abhishekgupta97023 <abhishekgupta97023@gmail.com>
2020-04-02 21:42:21 +0530

    Merge branch 'pranjal' of https://github.com/abhishekgupta-99/Sem6Notes
    # Conflicts:
    #	app/src/main/java/com/abhishek/SEM6/HomeActivity.java
    #	app/src/main/java/com/abhishek/SEM6/adapters/BookAdapter_db.java

--- app/src/main/java/com/abhishek/SEM6/adapters/BookAdapter_db.java
+++ app/src/main/java/com/abhishek/SEM6/adapters/BookAdapter_db.java
@@ -26,6 +26,7 @@ import com.abhishek.SEM6.DownloadService;
 import com.abhishek.SEM6.R;
 import com.abhishek.SEM6.models.Book;
 import com.abhishek.SEM6.models.Book_db;
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
@@ -74,7 +75,9 @@ public class BookAdapter_db extends RecyclerView.Adapter<BookAdapter_db.CustomVi
 
 
       if(!book.thumbnail.isEmpty()) {
-          Picasso.get().load(book.thumbnail.replace("http","https")).into(holder.ivChapter);
+         // Picasso.get().load(book.thumbnail.replace("http","https")).into(holder.ivChapter);
+
+          Glide.with(context).load(book.thumbnail).into(holder.ivChapter);
       }
         check_content_type(book,holder);
         holder.setItemLongClickListener(new ItemLongClickListener() {
